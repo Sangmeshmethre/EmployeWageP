ecb81447ef56e20ca7f2d7d50bd2b4e0bb83a95d
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 16:53:13 +0530

    Calculate Working day

---
+++ EmployeWage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeProgramUC5
+{
+    public class EmployeWage
+    {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAY = 2;
+        public static void WrokingWage()
+        {
+            //Variable
+            int empHrs = 0, empWage = 0, totalEmpWage=0;
+            //Computation
+            for (int day = 0; day < NUM_OF_WORKING_DAY; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(3);
+                    switch(empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                        case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                        default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs + EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Emp Wage :" + empWage);
+            }
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
+        }
+    }
+}
