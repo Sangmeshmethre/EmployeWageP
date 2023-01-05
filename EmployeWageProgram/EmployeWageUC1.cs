d72606792800a735d49e72239f35ff6f30d64446
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 16:18:00 +0530

    Check Employe Status

---
+++ EmployeWageProgram/EmployeWageUC1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWageProgram
+{
+    public class EmployeWageUC1
+    {
+        public static void EmployeWage()
+        {
+            //Constant 
+            int  IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation 
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+
+            }
+        }
+    }
+}
