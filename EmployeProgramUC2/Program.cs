09a65dd1f8721bfebfed34dc0c6193743dd0bbd2
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-05 16:25:46 +0530

    Calculate Employe Wage

---
+++ EmployeProgramUC2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeProgramUC2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeWageUC2.CalculateDaily();
+
+        }
+    }
+}
