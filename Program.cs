5110e705c13d56d0b25fc351cd055a76b7ea5738
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-06 23:19:39 +0530

    Employe Wage Program

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static EmployeProgramUC9.EmployeWageP;
+
+namespace EmployeProgramUC9
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Relience", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.ToString());
+        }
+    }
+}
