c84993a29b479e7dd2c22d73ec351e36a54ae1c6
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-06 23:28:40 +0530

    Multiple Companies Employe Wage

---
+++ IComputeEmpWage.cs
@@ -0,0 +1,8 @@
+﻿namespace EmployeProgramUC11
+{
+    public interface IComputeEmpWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+        public void computeEmpWage();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeProgramUC11
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
