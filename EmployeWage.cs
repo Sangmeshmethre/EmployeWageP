be5a254465451602f19625b74c185005f6af734d
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-06 23:16:24 +0530

    Employe Wage Program

---
+++ EmployeWage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWageUC8
+{
+    public  class EmployeWage
+    {
+        public const int IS_PART_TIME = 3;
+        public const int IS_FULL_TIME = 2;
+        public static int computrEmpWage(string company, int empRatePerHour, int numofworkingday, int maxiHoursPerMonth)
+        {
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDAys = 0;
+            while (totalEmpHrs <= maxiHoursPerMonth && totalWorkingDAys < numofworkingday)
+            {
+                totalWorkingDAys++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDAys + "Emp Hrs :" + empHrs);
+            }
+            int totalEmpWage = totalEmpHrs * empRatePerHour;
+            Console.WriteLine("Total Emp Wage for company : " + company + "is: " + totalEmpWage);
+            return totalEmpWage;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeWageUC8
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeWage.computrEmpWage("DMart", 20, 2, 10);
+            EmployeWage.computrEmpWage("Reliance", 10, 4, 20);
+        }
+    }
+}
