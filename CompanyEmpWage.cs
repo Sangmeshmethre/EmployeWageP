267bc5ef0e0a9280c5644c9c98ef38878b604c41
Sangmesh Methre <ssmcmethre3080@gmail.com>
2023-01-06 23:33:04 +0530

    Multipale Companeis Mange Employe

---
+++ CompanyEmpWage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeProgramUC12
+{
+    public class CompanyEmpWage
+    {
+        public string company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWages;
+
+        public CompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+        }
+
+        public void setTotalEmpWage(int totalEmpWages)
+        {
+            this.totalEmpWages = totalEmpWages;
+        }
+
+        public string toString()
+        {
+            return "Total emp wages for company:" + this.company + " is " + this.totalEmpWages;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeProgramUC12
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+            empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+            empWageBuilder.computeEmpWage();
+        }
+    }
+}
